4ae6891ab1a53d0eae616dd2505cc740c1680fc4
Hari Sirikonda <harisirikonda22@gmail.com>
2025-02-04 20:07:45 +0530

    Fetched data from Backend

--- frontend/src/Pages/ChatsPage.js
+++ frontend/src/Pages/ChatsPage.js
@@ -1,12 +1,28 @@
-import React from 'react'
+import axios from 'axios';
+import React, { useEffect, useState } from 'react';
 
 function ChatsPage() {
+  const [chats, setChats] = useState([]);
+
+  const fetchChats = async () => {
+    try {
+      const { data } = await axios.get("/api/chat");
+      setChats(data);
+      console.log(data);
+    } catch (error) {
+      console.error("Error fetching chats:", error);
+    }
+  };
+
+  useEffect(() => {
+    fetchChats();
+  }, []);
+
   return (
     <div>
       ChatsPage
     </div>
-  )
+  );
 }
 
-export default ChatsPage
-
+export default ChatsPage;
