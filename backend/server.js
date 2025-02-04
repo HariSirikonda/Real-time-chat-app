b3c95309168696a65ee97d90b463ac897f9970fa
Hari Sirikonda <harisirikonda22@gmail.com>
2025-02-04 18:00:37 +0530

    Added single chat end point

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,23 @@
 const express = require("express");
+const { chats } = require("./data/data");
+const dotenv = require("dotenv");
+
 const app = express()
+dotenv.config()
 
 app.get("/", (req, res) => {
     res.send("API is Running");
 });
-app.listen(5000, console.log("Server started on port 5000"));
+
+app.get("/api/chat", (req, res) => {
+    res.send(chats);
+});
+
+app.get("/api/chat/:id", (req, res) => {
+    const singleChat = chats.find((c) => c._id === req.params.id);
+    res.send(singleChat)
+});
+
+const PORT = process.env.PORT || 5000
+
+app.listen(PORT, console.log(`Server started on port ${PORT}`));
